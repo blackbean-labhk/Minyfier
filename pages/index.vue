4eb14762c618ea081a1fdbfdb3f9993a867672a0
blackbean-labhk <151385500+blackbean-labhk@users.noreply.github.com>
2024-09-02 12:40:54 +0530

    design fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="flex flex-col justify-center items-center min-h-screen bg-gray-100">
+  <main class="flex flex-col justify-center items-center min-h-[85vh]">
     <div class="text-center">
       <h1 class="text-4xl font-bold mb-4">MinyVinyl</h1>
       <p class="text-lg mb-8">
@@ -8,10 +8,16 @@
         We deliver true ownership with digital playlists, and superior IRL listening experiences.
       </p>
       <div class="flex flex-col space-y-4">
-        <button class="bg-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-600 transition duration-300">
+        <button
+          class="bg-blue-500 font-bold text-white px-6 py-3 rounded-md hover:bg-blue-600 transition duration-300"
+          @click="goToMinyfy"
+        >
           MINYFY YOUR PLAYLIST
         </button>
-        <button class="bg-green-500 text-white px-6 py-3 rounded-md hover:bg-green-600 transition duration-300">
+        <button
+          class="bg-green-500 font-bold text-white px-6 py-3 rounded-md hover:bg-green-600 transition duration-300"
+          @click="goToCreateEPK"
+        >
           CREATE AN EPK
         </button>
       </div>
@@ -21,3 +27,17 @@
     </div>
   </main>
 </template>
+
+<script>
+export default {
+  methods: {
+    goToMinyfy() {
+      window.location.href = 'https://minyfy.subwaymusician.xyz';
+    },
+    goToCreateEPK() {
+      // Replace with the appropriate URL for creating an EPK
+      window.location.href = 'https://miny.subwaymusician.xyz';
+    },
+  },
+};
+</script>
