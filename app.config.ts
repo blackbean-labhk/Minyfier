02af62a1aae837be8a1f6677f4de1becc875ead8
collectivewin <hello@collectivewin.ca>
2024-10-25 23:18:46 +0530

    meta fix

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   title: 'Minyfier',
-  image: 'https://sink.cool/banner.png',
+  image: 'https://go.minyvinyl.com/icon.png',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
